03499384c7747855fa928ca9375d86dc541d808c
Timen van Gelderen <tvgelderen@outlook.com>
2022-11-29 18:56:30 +0100

    stipe working

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useAuthContext } from '../context/AuthContext';
 import Plans from '../components/Plans';
@@ -7,7 +7,7 @@ const Profile: React.FC = () => {
     const { logOut } = useAuthContext();
     
     return (
-        <div className="w-full min-h-screen flex justify-center items-center">
+        <div className="w-full h-screen flex justify-center pt-[90px]">
             <div className='w-[95%] max-w-[700px] m-auto'>
                 <h1 className='text-4xl md:text-5xl lg:text-6xl font-semibold text-left pb-2 border-b-2 border-[#202020]'>Edit Profile</h1>
                 <div className='grid grid-cols-6 py-4 px-2'>
@@ -20,13 +20,13 @@ const Profile: React.FC = () => {
                           className=''
                         />
                     </div>
-                    <div className='col-span-5 mx-4'>
-                        <p className='text-xl font-semibold pb-1 border-b-2 border-[#202020]'>Subscription Plans</p>
+                    <div className='col-span-5'>
+                        <p className='text-2xl font-semibold pb-1 border-b-2 border-[#202020]'>Subscription Plans</p>
                         <p className='text-[#aaaaaa] py-2'>Renewal date: (dd/mm/yyyy)</p>
 
                         <Plans />
 
-                        <button className='w-full py-2 mt-8 rounded bg-[#e50914]' onClick={logOut}>
+                        <button className='w-full py-2 mt-8 text-lg font-semibold rounded bg-[#e50914]' onClick={logOut}>
                             Sign Out
                         </button>
                     </div>
